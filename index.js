005a5c94744a29444cbbde7578309d2f4d6f012d
KarinKwint <83405911+KarinKwint@users.noreply.github.com>
2021-06-18 12:45:25 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+// even-odd reporter
+function isEven(i) {
+    return n % 2 === 0
+}
+console.log(isEven);
+
+for (i = 0; i < 20; i++) {
+    if (i = isEven) {
+        return i + "this is an even number";
+    } else {
+        return i + "this is an odd number";
+    };
+}
+// Multiplication tables
+function Multiplication(score) {
+    console.log(i * 9);
+}
+console.log(Multiplication);
+
+for (let i = 0; i <= 10; 1++) {
+    console.log(i + Multiplication + "");
+}
+// Grade Assigment
+function assignGrade(score) {
+    if (score > 90) {
+        return 'A';
+    } else if (score > 80) {
+        return 'B';
+    } else if (score > 70) {
+        return 'C';
+    } else if (score > 65) {
+        return 'D';
+    } else {
+        return 'E';
+    }
+}
+console.log(assignGrade);
+
+for (let i = 60; i <= 100; 1++) {
+    console.log("Voor " + i + " punten, krijg je een ");
+}
